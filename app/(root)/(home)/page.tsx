18f85f0a2dd0bcb6fe71c0bfeddfdf0556cff405
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-08-10 16:16:01 +0530

    Homepage made

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,69 @@
+"use client";
+
+import Image from "next/image";
+import { useUser } from "@clerk/nextjs";
+import MeetingTypeList from "@/components/MeetingTypeList";
+
 const Home = () => {
+  const { user } = useUser();
+
+  const now = new Date();
+  const hours = now.getHours();
+
+  const getGreeting = () => {
+    if (hours < 12) return "Good morning ☀️";
+    if (hours < 18) return "Good afternoon 🌤️";
+    return "Good evening 🌙";
+  };
+
+  const getTimeEmoji = () => {
+    if (hours < 6) return "🌌";
+    if (hours < 12) return "🌞";
+    if (hours < 18) return "☀️";
+    return "🌆";
+  };
+
+  const time = now.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  const date = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+    now
+  );
+
   return (
-    <section className="flex size-full flex-col gap-10 text-white">
-      <h1 className="text-3xl font-bold">
-        Home
-      </h1>
+    <section className="flex size-full flex-col gap-5 text-white">
+      <div className="relative h-[303px] w-full rounded-[20px] overflow-hidden">
+        {/* Background Image */}
+        <Image
+          src="/images/hero-background.png"
+          alt="Hero background"
+          fill
+          className="object-cover"
+          priority
+        />
+
+        {/* Overlay */}
+        <div className="absolute inset-0 flex flex-col justify-between bg-black/30 max-md:px-5 max-md:py-8 lg:p-11">
+          {/* Dynamic Greeting */}
+          <h2 className="glassmorphism max-w-max rounded font-sans py-2 px-4 text-center text-6xl font-extrabold">
+            {getGreeting()} {user?.firstName || "Guest"}
+          </h2>
+
+          {/* Time & Date */}
+          <div className="flex flex-col gap-2 ml-4 font-sans">
+            <h1 className="text-4xl font-bold lg:text-3xl">
+              {time} {getTimeEmoji()} 
+              <span className="text-slate-600 text-lg">Reload the page for updating clock</span>
+            </h1>
+            <p className="text-lg font-medium text-sky-1 lg:text-xl">{date}</p>
+          </div>
+        </div>
+      </div>
+
+      <MeetingTypeList />
     </section>
-  )
-}
-export default Home
+  );
+};
+
+export default Home;
---
+++ components/MeetingModal.tsx
@@ -0,0 +1,82 @@
+"use client"
+
+import { ReactNode } from "react";
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+} from "@/components/ui/dialog";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { cn } from "@/lib/utils";
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
+
+interface MeetingModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  className?: string;
+  children?: ReactNode;
+  handleClick?: () => void;
+  buttonText?: string;
+  instantMeeting?: boolean;
+  image?: string;
+  buttonClassName?: string;
+  buttonIcon?: string;
+}
+
+const MeetingModal = ({
+  isOpen,
+  onClose,
+  title,
+  className,
+  children,
+  handleClick,
+  buttonText,
+  instantMeeting,
+  image,
+  buttonClassName,
+  buttonIcon,
+}: MeetingModalProps) => {
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent className="flex w-full max-w-[520px] flex-col gap-6 border-none bg-black px-6 py-9 text-white">
+        <DialogHeader>
+          <VisuallyHidden>
+            <DialogTitle>{title}</DialogTitle>
+          </VisuallyHidden>
+        </DialogHeader>
+        <div className="flex flex-col gap-6">
+          {image && (
+            <div className="flex justify-center">
+              <Image src={image} alt="checked" width={72} height={72} />
+            </div>
+          )}
+          <h1 className={cn("text-3xl font-bold leading-[42px]", className)}>
+            {title}
+          </h1>
+          {children}
+          <Button
+            className={
+              "bg-purple-600 focus-visible:ring-0 focus-visible:ring-offset-0 cursor-pointer hover:bg-purple-500 hover:text-white"
+            }
+            onClick={handleClick}
+          >
+            {buttonIcon && (
+              <Image
+                src={buttonIcon}
+                alt="button icon"
+                width={13}
+                height={13}
+              />
+            )}{" "}
+            &nbsp;
+            {buttonText || "Schedule Meeting"}
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+export default MeetingModal;
