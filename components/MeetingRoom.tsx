f3e9a065d120c241383d65148ae6bfda2fbc1d95
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-08-16 00:22:25 +0530

    Do it

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -28,10 +28,10 @@ type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 const MeetingRoom = () => {
   const searchParams = useSearchParams();
   const isPersonalRoom = !!searchParams.get("personal");
-  const router = useRouter();
   const [layout, setLayout] = useState<CallLayoutType>("speaker-left");
   const [showParticipants, setShowParticipants] = useState(false);
   const { useCallCallingState } = useCallStateHooks();
+  const router = useRouter();
 
   const callingState = useCallCallingState();
 
